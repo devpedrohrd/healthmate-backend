8ae8f6bd6031f912220609322b4601dfd823c86f
Pedrohrd <pedrohrd87@gmail.com>
2024-08-14 15:19:50 -0300

    feat: login com google

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,47 @@
 import { serve } from "@hono/node-server";
 import { swaggerUI } from "@hono/swagger-ui";
 import { Hono } from "hono";
-import { bearerAuth } from "hono/bearer-auth";
-import { setCookie } from "hono/cookie";
 import { cors } from "hono/cors";
 import { csrf } from "hono/csrf";
 import { logger } from "hono/logger";
 import { secureHeaders } from "hono/secure-headers";
 
 import swagger from "../swagger.json";
+import { avaliacaoRoutes } from "./routes/avaliacao";
 import { lembreteRoutes } from "./routes/lembrete";
 import { loginRoutes } from "./routes/login";
+import { routeLoginGoogle } from "./routes/loginGoogle";
 import { medicamentRoutes } from "./routes/medicament";
 import { pacientRoutes } from "./routes/pacient";
 import { profissionalRoutes } from "./routes/profissional";
+import { relatorioRoutes } from "./routes/relatorio";
 
 const app = new Hono();
 
 app.use(logger());
 app.use(
+  "*",
   cors({
-    origin: "http://127.0.0.1:5500",
+    origin: Bun.env.ORIGIN as string,
     credentials: true,
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 app.use(csrf());
 app.use(secureHeaders());
 
-app
-  .get("/", (c) => {
-    return c.text("API rodando! 🚀👌");
-  })
-  .get("/set-cookie", async (c) => {
-    setCookie(c, "token", "cookie", {
-      httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
-    });
-    return c.text("Cookie set successfully!");
-  });
+// app.route("/login", loginRoutes);
+app.route("/pacientes", pacientRoutes);
+app.route("/medicamentos", medicamentRoutes);
+app.route("/profissional", profissionalRoutes);
+app.route("/lembretes", lembreteRoutes);
+app.route("/avaliacao", avaliacaoRoutes);
+app.route("/", routeLoginGoogle);
+app.route("/relatorio", relatorioRoutes);
 
-app.route("/api/login", loginRoutes);
-app.route("/api/pacientes", pacientRoutes);
-app.route("/api/medicamentos", medicamentRoutes);
-app.route("api/profissional", profissionalRoutes);
-app.route("/api/lembretes", lembreteRoutes);
-
-app.get("/api/docs", swaggerUI({ url: "/api/swagger" }));
-app.get("/api/swagger", async (c) => {
+app.get("/docs", swaggerUI({ url: "/swagger" }));
+app.get("/swagger", async (c) => {
   return c.json(swagger);
 });
 
