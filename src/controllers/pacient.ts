f36e210dc0db404036a0533ace734b6574340af7
Pedrohrd <pedrohrd87@gmail.com>
2024-08-16 11:52:15 -0300

    feat: add filtros do paciente

--- src/controllers/pacient.ts
+++ src/controllers/pacient.ts
@@ -1,8 +1,8 @@
-import { pacienteSchema } from "../schema/schemas";
 import { Context } from "hono";
 import { ZodError } from "zod";
 
 import { prisma } from "../config/Prisma";
+import { pacienteSchema } from "../schema/schemas";
 
 export const getPacients = async (c: Context) => {
   try {
@@ -37,7 +37,7 @@ export const createPacient = async (c: Context) => {
     }
 
     await prisma.paciente.create({
-      data: parsedData,
+      data: { ...parsedData, createdAt: new Date().toLocaleString() },
     });
 
     return c.json({ message: "Paciente criado com sucesso" }, 201);
@@ -137,5 +137,111 @@ export const deletePacient = async (c: Context) => {
     return c.json({ message: "Paciente deletado !" }, 200);
   } catch (err) {
     return c.json({ error: "Internal Server Error !" }, 500);
+  }
+};
+
+export const getMedicamentsByPacient = async (c: Context) => {
+  try {
+    const { id } = c.req.param();
+
+    const pacient = await prisma.paciente.findUnique({
+      where: {
+        id: Number(id),
+      },
+      include: {
+        Medicamento: true,
+      },
+    });
+
+    if (!pacient) {
+      return c.json({ error: "Paciente não encontrado" }, 404);
+    }
+
+    if (pacient.Medicamento.length === 0) {
+      return c.json({ error: "Nenhum medicamento encontrado" }, 404);
+    }
+
+    return c.json(pacient.Medicamento, 200);
+  } catch (err) {
+    return c.json({ error: "Internal Server Error !" }, 500);
+  }
+};
+
+export const getPacientByEmail = async (c: Context) => {
+  try {
+    const body = await c.req.json();
+
+    const pacient = await prisma.paciente.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (!pacient) {
+      return c.json({ error: "Paciente não encontrado" }, 404);
+    }
+
+    return c.json(pacient, 200);
+  } catch (err) {
+    return c.json({ error: "Internal Server Error !" }, 500);
+  }
+};
+
+export const getPacientByName = async (c: Context) => {
+  try {
+    const { nome } = await c.req.json();
+
+    const pacients = await prisma.paciente.findMany({
+      where: {
+        nome: {
+          contains: nome,
+        },
+      },
+    });
+
+    if (pacients.length === 0) {
+      return c.json({ error: "Nenhum paciente encontrado" }, 404);
+    }
+
+    return c.json(pacients, 200);
+  } catch (err) {
+    return c.json({ error: "Internal Server Error !" }, 500);
+  }
+};
+
+export const getpacientByData = async (c: Context) => {
+  try {
+    const { data } = await c.req.json();
+
+    const dataInicio = `${data}, 00:00:00`;
+    const dataFim = `${data}, 23:59:59`;
+
+    console.log("Data Início:", dataInicio);
+    console.log("Data Fim:", dataFim);
+
+    const profissional = await prisma.paciente.findMany({
+      where: {
+        createdAt: {
+          gte: dataInicio,
+          lte: dataFim,
+        },
+      },
+      select: {
+        id: true,
+        nome: true,
+        email: true,
+        telefone: true,
+        createdAt: true,
+      },
+    });
+
+    if (!profissional || profissional.length === 0) {
+      return c.json({ message: "Profissional não encontrado" }, 404);
+    }
+
+    return c.json(profissional, 200);
+  } catch (e) {
+    console.error("Erro:", e);
+    return c.json({ message: "Erro interno" }, 500);
   }
 };
--- src/routes/pacient.ts
+++ src/routes/pacient.ts
@@ -1,6 +1,16 @@
 import { Hono } from "hono";
 
-import { createPacient, deletePacient, getPacientById, getPacients, updatePacient } from "../controllers/pacient";
+import {
+  createPacient,
+  deletePacient,
+  getMedicamentsByPacient,
+  getpacientByData,
+  getPacientByEmail,
+  getPacientById,
+  getPacientByName,
+  getPacients,
+  updatePacient,
+} from "../controllers/pacient";
 
 const pacientRoutes = new Hono();
 
@@ -9,5 +19,9 @@ pacientRoutes.post("/", (c) => createPacient(c));
 pacientRoutes.get("/:id", (c) => getPacientById(c));
 pacientRoutes.put("/:id", (c) => updatePacient(c));
 pacientRoutes.delete("/:id", (c) => deletePacient(c));
+pacientRoutes.get("/:id/medicaments", (c) => getMedicamentsByPacient(c));
+pacientRoutes.post("/email", (c) => getPacientByEmail(c));
+pacientRoutes.post("/nome", (c) => getPacientByName(c));
+pacientRoutes.post("/data", (c) => getpacientByData(c));
 
 export { pacientRoutes };
