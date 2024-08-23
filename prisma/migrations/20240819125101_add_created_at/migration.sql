197a47d6c75d7d13cb651ec8506fe9cecdf8db32
Pedrohrd <pedrohrd87@gmail.com>
2024-08-23 09:48:31 -0300

    .

---
+++ prisma/migrations/20240819125101_add_created_at/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "AvaliacaoSaude" ADD COLUMN     "createdAt" VARCHAR(255);
+
+-- AlterTable
+ALTER TABLE "Lembrete" ADD COLUMN     "createdAt" VARCHAR(255);
+
+-- AlterTable
+ALTER TABLE "Medicamento" ADD COLUMN     "createdAt" VARCHAR(255);
+
+-- AlterTable
+ALTER TABLE "Relatorio" ADD COLUMN     "createdAt" VARCHAR(255);
